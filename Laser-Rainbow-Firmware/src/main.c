10ba1e40e8698bfcc7353fc1c5a7c7d629599f49
sesj <sebastian.sjodin@24hr.se>
2012-12-17 22:20:01 +0100

    Implemented some kind of USB interface

--- Laser-Rainbow-Firmware/src/main.c
+++ Laser-Rainbow-Firmware/src/main.c
@@ -9,23 +9,45 @@
  * Include header files for all drivers that have been imported from
  * Atmel Software Framework (ASF).
  */
-#include <asf.h>
+//#include <asf.h>
+//#include "board.h"
+#include <avr/io.h>
+#include "config/compiler.h"
+#include "usb/usb_standard_request.h"
 
-#define tbi(x,y) x ^= _BV(y) //toggle bit - using bitwise XOR operator
+#define tbi(x,y) x ^= _BV(y)		//toggle bit - using bitwise XOR operator
+
+void board_init();
+
+void sof_action()
+{
+	//cpt_sof++;
+}
 
 int main (void)
 {
 	board_init();
 
+	 //Usb_enable_regulator();
+
 	// Insert application code here, after the board has been initialized.
-	PORTB_DIR = 0xa8;				// PORTB as OUTPUT
-	PORTB_OUT = 0b01001010;				// All pins of PORTB LOW
-
-	PORTC_DIR = 0x4c;				// PORTB as OUTPUT
-	PORTC_OUT = 0b01001010;				// All pins of PORTB LOW
-
-	unsigned int i;
-	while(1 == 1)					//Infinite loop
+	//PORTA_DIR = 0xff;					// PORTB as OUTPUT
+	//PORTA_OUT = 0b01001010;				// All pins of PORTB LOW
+	//
+	//PORTB_DIR = 0xff;					// PORTB as OUTPUT
+	//PORTB_OUT = 0b01001010;				// All pins of PORTB LOW
+//
+	//PORTC_DIR = 0xff;					// PORTB as OUTPUT
+	//PORTC_OUT = 0b01001010;				// All pins of PORTB LOW
+//
+	//PORTD_DIR = 0xff;					// PORTB as OUTPUT
+	//PORTD_OUT = 0b01001010;				// All pins of PORTB LOW
+//
+//
+	//PORTA_OUT = 0x00;
+	//
+	//unsigned int i = 0;
+	while(1 == 1)						//Infinite loop
 	{
 	//
 		//PORTB_OUT = 0x0f;				// All pins of PORTB LOW
@@ -39,3 +61,16 @@ int main (void)
 		////tbi(PORTB, PB0);			//here the toggling takes place
 	}	
 }
+
+void board_init()
+{
+}
+
+void configurePorts()
+{
+}
+
+void configureUsb()
+{
+}
+
---
+++ Laser-Rainbow-Firmware/src/usb/commands.h
@@ -0,0 +1,22 @@
+/*
+ * commands.h
+ *
+ * Created: 2012-12-17 19:00:14
+ */ 
+
+
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+
+// Laser Rainbow USB commands
+#define  SETUP_PIN1                   0x00
+#define  SETUP_PIN2                   0x01
+#define  SETUP_PIN3                   0x02
+#define  SETUP_PIN4                   0x03
+#define  SETUP_PIN5                   0x04
+#define  SETUP_PIN6                   0x05
+#define  DEBUG_DERPER                 0xde
+
+
+#endif /* COMMANDS_H_ */
