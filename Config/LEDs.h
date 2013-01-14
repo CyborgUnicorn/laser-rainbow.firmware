34f2d97e882cc0e37491852ba17b39bbace1f7c7
Johan Öbrink <johan.obrink@gmail.com>
2013-01-14 21:09:05 +0100

    Updated ignore

--- Config/LEDs.h
+++ Config/LEDs.h
@@ -71,8 +71,10 @@
 			/** LED mask for the first LED on the board. */
 			#define LEDS_LED1        (1 << 1)
 
+			#define LEDS_LED2		 (1 << 7)
+
 			/** LED mask for all the LEDs on the board. */
-			#define LEDS_ALL_LEDS    LEDS_LED1
+			#define LEDS_ALL_LEDS    (LEDS_LED1 | LEDS_LED2)
 
 			/** LED mask for the none of the board LEDs. */
 			#define LEDS_NO_LEDS     0
